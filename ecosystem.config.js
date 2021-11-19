07bb2c0bfb64fba262cb0b818fa3e324c16252cb
tonythrefreedom <tonymustbegreat@gmail.com>
2021-11-19 15:53:51 +0900

    docker file and docker compose updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       // pm2로 실행한 프로세스 목록에서 이 애플리케이션의 이름으로 지정될 문자열
-      name: "staking4u",
+      name: "nxdf",
       // pm2로 실행될 파일 경로
       script: "./bin/www",
       watch: false,
