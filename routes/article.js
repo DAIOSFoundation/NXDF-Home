7c8c9a004582d231c8c42591b4cdf00741e7cc0c
tonythrefreedom <tonymustbegreat@gmail.com>
2021-11-19 19:47:45 +0900

    changed some old contents into new contents

--- routes/article.js
+++ routes/article.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('article', { title: 'Staking4u' });
+  res.render('article', { title: 'NXDF Protocol' });
 });
 
 router.get('/detail/1', function(req, res, next) {
-  res.render('article-01', { title: 'Staking4u' });
+  res.render('article-01', { title: 'NXDF Protocol' });
 });
 
 router.get('/detail/2', function(req, res, next) {
-  res.render('article-02', { title: 'Staking4u' });
+  res.render('article-02', { title: 'NXDF Protocol' });
 });
 
 router.get('/detail/3', function(req, res, next) {
-  res.render('article-03', { title: 'Staking4u' });
+  res.render('article-03', { title: 'NXDF Protocol' });
 });
 
 router.get('/detail/4', function(req, res, next) {
-  res.render('article-04', { title: 'Staking4u' });
+  res.render('article-04', { title: 'NXDF Protocol' });
 });
 module.exports = router;
